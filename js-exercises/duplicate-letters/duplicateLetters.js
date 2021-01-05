be50fbb7661e22e52475b8d7acacbd1cd0f2edf9
nitinmadelyn <nitin@Nitins-MacBook-Pro.local>
2021-01-05 21:17:01 +0530

    refactor code

--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -1,20 +1,23 @@
-function duplicateLetters(...args) {
-  if (typeof args[0] !== 'string' || args.length > 1) {
+function duplicateLetters(letters) {
+  if (typeof letters !== 'string') {
     return 'Not a string';
   }
-  const str = args[0];
+
   let countArray = {};
-  for (const char in Object.values(str)) {
-    if (typeof countArray[str[char]] === 'undefined') {
-      countArray[str[char]] = 1;
+  for (const char in letters) {
+    if (typeof countArray[letters[char]] === 'undefined') {
+      countArray[letters[char]] = 1;
     } else {
-      countArray[str[char]] += countArray[str[char]];
+      countArray[letters[char]] += 1;
     }
   }
+
   countArray = Object.values(countArray);
   countArray.sort((a, b) => b - a);
   return (countArray[0] <= 1) ? false : countArray[0];
 }
+
+// console.log(duplicateLetters('asyfghjklqyerftyuiop'))
 
 export default {
   duplicateLetters,
