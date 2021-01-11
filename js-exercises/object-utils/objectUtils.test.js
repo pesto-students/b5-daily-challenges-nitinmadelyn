615d630ef2914bfe5b20b554e45f27c2f24081e2
nitinmadelyn <nitin@Nitins-MacBook-Pro.local>
2021-01-11 18:50:40 +0530

    assignment done

---
+++ js-exercises/object-utils/objectUtils.test.js
@@ -0,0 +1,23 @@
+import { map } from './objectUtils';
+
+// write your own test cases
+
+const mapObject = {name: "nitin", age: 10, gender: "male"};
+
+mapObject.map((key, value) => {
+    console.log(key, value)
+})
+
+
+
+
+public $default = array(
+    'datasource' => 'Database/Mysql',
+    'persistent' => false,
+    'host' => 'localhost',
+    'login' => 'vX8NRi7cD3667',
+    'password' => 'N+8x2}Ly5(8X5Hgk-n',
+    'database' => 'myproalign',
+    'prefix' => '',
+    //'encoding' => 'utf8',
+);
