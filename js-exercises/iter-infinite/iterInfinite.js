482f93d71ae0687ba9e0d89d69ac9abe7cbcd8e2
nitinmadelyn <nitin@Nitins-MacBook-Pro.local>
2021-01-19 23:01:18 +0530

    refactor code

--- js-exercises/iter-infinite/iterInfinite.js
+++ js-exercises/iter-infinite/iterInfinite.js
@@ -2,16 +2,16 @@
 Count function
 */
 function* count(...args) {
-  if (typeof args[0] !== 'number') {
-    throw new Error('Expected number, received - ' + typeof args[0]);
+  if (typeof value !== "number") {
+    throw new Error("Expected number, received - " + typeof value);
   }
 
-  if (args[1] && typeof args[1] !== 'number') {
-    throw new Error('Expected number, received - ' + typeof args[1]);
+  if (times && typeof times !== "number") {
+    throw new Error("Expected number, received - " + typeof times);
   }
 
-  let num = args[0];
-  let limit = args[1];
+  let num = value;
+  let limit = times;
 
   if (limit) {
     while (limit > 0) {
@@ -28,70 +28,26 @@ function* count(...args) {
 /*
 Cycle function
 */
-function* cycle(...args) {
-  if (typeof args[0] !== 'string') {
-    throw new Error('Expected string, received - ' + typeof args[0]);
-  }
-
-  if (args[1] && typeof args[1] !== 'number') {
-    throw new Error('Expected number, received - ' + typeof args[1]);
-  }
-
-  const inputString = args[0];
-  let noOfTimes = args[1];
-  let i = 0;
-
-  if (noOfTimes) {
-    while (true) {
-      yield inputString[i];
-
-      if (i === inputString.length - 1) {
-        i = 0;
-        --noOfTimes;
-        if (noOfTimes === 0) {
-          return;
-        }
-      } else {
-        i++;
-      }
-    }
-  } else {
-    while (true) {
-      yield inputString[i];
-      if (i === inputString.length - 1) {
-        i = 0;
-      } else {
-        i++;
-      }
-    }
+function* cycle(iter, times = Infinity) {
+  while (times > 0) {
+    yield* iter;
+    times--;
   }
 }
 
 /* 
 Repeat function
 */
-function* repeat(...args) {
-  if (typeof args[0] !== 'number') {
-    throw new Error('Expected number, received - ' + typeof args[0]);
+function* repeat(value, times = Infinity) {
+  if (typeof value !== "number") {
+    throw new Error("Expected number, received - " + typeof value);
   }
 
-  if (args[1] && typeof args[1] !== 'number') {
-    throw new Error('Expected number, received - ' + typeof args[1]);
+  if (times && typeof times !== "number") {
+    throw new Error("Expected number, received - " + typeof times);
   }
 
-  const num = args[0];
-  let repeatTimes = args[1];
-
-  if (repeatTimes) {
-    while (repeatTimes > 0) {
-      --repeatTimes;
-      yield num;
-    }
-  } else {
-    while (true) {
-      yield num;
-    }
-  }
+  return yield* cycle([value], times);
 }
 
 export { count, cycle, repeat };
